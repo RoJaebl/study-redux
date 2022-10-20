5b637b067c172fd0a08e38c298260036e360990b
RoJaebl <qgs0924@gmail.com>
2022-10-20 14:04:57 +0900

    :zap: Reducer code stabilization

--- src/index.js
+++ src/index.js
@@ -4,26 +4,25 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  const { type } = action;
-  if (type === "ADD") {
-    return (count += 1);
-  } else if (type === "MINUS") {
-    return (count -= 1);
-  } else return count;
+  switch (action.type) {
+    case ADD:
+      return (count += 1);
+    case MINUS:
+      return (count -= 1);
+    default:
+      return count;
+  }
 };
 
 const countStore = createStore(countModifier);
-console.log(countStore);
 const onChange = () => (number.innerText = countStore.getState());
 countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
-};
-const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
-};
+const handleAdd = () => countStore.dispatch({ type: ADD });
+const handleMinus = () => countStore.dispatch({ type: MINUS });
 
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
