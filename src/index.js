9c244db91f36f20cce7383a8f2b01ad19084c7de
RoJaebl <qgs0924@gmail.com>
2022-10-20 16:24:28 +0900

    :hammer: Start 'ToDo' program

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+const reducer = (state = "", { type }) => {
+  console.log(type);
+};
+const store = createStore(reducer);
 
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return (count += 1);
-    case MINUS:
-      return (count -= 1);
-    default:
-      return count;
-  }
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: toDo });
 };
 
-const countStore = createStore(countModifier);
-const onChange = () => (number.innerText = countStore.getState());
-countStore.subscribe(onChange);
-const handleAdd = () => countStore.dispatch({ type: ADD });
-const handleMinus = () => countStore.dispatch({ type: MINUS });
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
